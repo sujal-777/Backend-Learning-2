03ca47cfb4ebe86139b83ac7e6862582e543f5c3
Sujal <palsujal707@gmail.com>
2024-11-24 17:23:55 +0530

    commiting files

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const app = express();
+
+app.get ('/', (req,res) => {
+    res.send('Server is ready')
+});
+
+// get a list of 5 jokes
+
+app.get('/api/jokes', (req,res) => {
+    const jokes = [
+        {
+            id : 1,
+            title : "A joke",
+            content : "This is a joke"
+        },
+        {
+            id : 2,
+            title : "Another Joke",
+            content : "THis is another Joke"
+        },
+        {
+            id : 3,
+            title : "A third joke",
+            content : "this is third joke"
+        }
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+ 
+app.listen(port , () =>{
+    console.log(`Server is ready at http://localhost:${port}`)
+});
+
